13ea23a7463315a91341198e04271e625884f59b
sihadan <def6488@gmail.com>
2019-10-23 16:40:19 +0900

    implement github issue related functions

--- run.py
+++ run.py
@@ -3,57 +3,31 @@ import music
 import time
 import tweet
 import validation
-
-USED_LIST = "used_song_list.txt"
-TWEET_LENGTH_LIMIT = 140
-
+import issue
+import config
 
 # check is song is already posted before
-def is_song_duplicated(track_id):
+def duplicated(track_id):
 
-    with open(USED_LIST, "r") as f:
-        used_songs = [l.strip() for l in f.readlines()]
+    used_track_ids = [
+        t.strip()
+        for t in issue.get_issue(config.GITHUB_REPO_URL, config.GITHUB_ISSUE_ID).split()
+    ]
 
-        if track_id in used_songs:
-            return True
+    if track_id in used_track_ids:
+        return True
 
     return False
 
 
-# update the used song list file
-def update_used_song_list(track_id, max_list_size=50):
-
-    with open(USED_LIST, "r") as f:
-        lines = f.readlines()
-
-    if len(lines) > max_list_size:
-        lines = lines[-max_list_size:]
-
-    with open(USED_LIST, "w") as f:
-        f.writelines(lines)
-        f.write(track_id + "\n")
-
-
-# return song that contains lyric data
-def select_song(track_list):
-
-    while True:
-        track = random.choice(track_list)
-
-        # if that song is already used
-        if is_song_duplicated(track):
-            continue
-
-        song = music.get_song(track)
-
-        # if it contains lyric data
-        if song:
-            return song, track
+# update the used song list
+def update_used_song_list(track_id):
+    issue.update_issue(config.GITHUB_REPO_URL, config.GITHUB_ISSUE_ID, str(track_id))
 
 
 # separate verses from lyric
 # if min_len is given, only verses longer than min_len is returned
-def separate_verse(lyric, min_len=0, max_len=99999):
+def separate_verses(lyric, min_len=0, max_len=99999):
     splited = lyric.split("\n\n")
 
     splited = [s for s in splited if min_len <= len(s) <= max_len]
@@ -61,37 +35,54 @@ def separate_verse(lyric, min_len=0, max_len=99999):
     return splited
 
 
+# select a verse from a song
+def select_verse(song, min_len, max_len):
+
+    # separate lyric to verses
+    verses = separate_verses(
+        song["lyric"],
+        min_len=min_len,
+        max_len=max_len - len(song["title"]) - len(song["artist"]) - 10,
+    )
+
+    # if no verse that pass filter
+    if not verses:
+        raise ValueError
+
+    verse = random.choice(verses)
+
+    # check english/korean ratio of the lyric
+    if not validation.is_lyric_native(song["lyric"], ratio=0.2):
+        raise ValueError
+
+    # check english/korean ratio of the verse
+    if not validation.is_lyric_native(verse, ratio=0.1):
+        raise ValueError
+
+    # check special chracters in verse
+    if not validation.is_lyric_pure(verse):
+        raise ValueError
+
+    return verse
+
+
 # get random verse from naver music
 def get_random_verse(min_len=0, max_len=99999):
     track_list = music.get_song_list()
 
     while True:
-        song, track_id = select_song(track_list)
-        lyric = song["lyric"]
-        title = song["title"]
-        artist = song["artist"]
+        track_id = random.choice(track_list)
 
-        # check english/korean ratio of the lyric
-        if not validation.is_lyric_native(lyric, ratio=0.2):
+        # if this song had been already used
+        if duplicated(track_id):
             continue
 
-        # separate lyric to verses
-        verses = separate_verse(
-            lyric, min_len=min_len, max_len=max_len - len(title) - len(artist) - 10
-        )
-
-        # if no verse that pass filter
-        if not verses:
-            continue
-
-        verse = random.choice(verses)
-
-        # check english/korean ratio of the verse
-        if not validation.is_lyric_native(verse, ratio=0.1):
-            continue
-
-        # check special chracters in verse
-        if not validation.is_lyric_pure(verse):
+        # return song that contains lyric/title/artist data
+        song = music.get_song(track_id)
+        try:
+            verse = select_verse(song, min_len, max_len)
+        except Exception as e:
+            print(e)
             continue
 
         # verse found
@@ -101,7 +92,7 @@ def get_random_verse(min_len=0, max_len=99999):
     return song["title"], song["artist"], verse
 
 
-# format song data to tweet
+# format song data for tweet
 def format_tweet(title, artist, verse):
     tweet = ""
     tweet += verse
@@ -111,14 +102,13 @@ def format_tweet(title, artist, verse):
     return tweet
 
 
-def run_bot():
-    title, artist, verse = get_random_verse(min_len=30, max_len=TWEET_LENGTH_LIMIT)
-    body = format_tweet(title, artist, verse)
-    tweet.post(body)
-
-
 def main():
-    run_bot()
+    title, artist, verse = get_random_verse(
+        min_len=30, max_len=config.TWEET_LENGTH_LIMIT
+    )
+    body = format_tweet(title, artist, verse)
+    print(body)
+    # tweet.post(body)
 
 
 if __name__ == "__main__":
